80f7e9ce5941f685f87def5c77e26371aa5f6869
Eric Fortis <efortis@gmail.com>
2025-06-15 15:12:13 -0400

    initial

---
+++ Formula/fcpscene.rb
@@ -0,0 +1,23 @@
+class Fcpscene < Formula
+  include Language::Python::Virtualenv
+
+  desc "Final Cut Pro scene cut detector"
+  homepage "https://github.com/ericfortis/final-cut-pro-scene-detect"
+  url "https://github.com/ericfortis/final-cut-pro-scene-detect/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "bb0500ede43fffff8e35119fd1b973eb44fa7e35ecde71c5a04b1b5e99fc7634"
+  license "MIT"
+
+  depends_on "python@3.11"
+  depends_on "ffmpeg"
+
+  def install
+    virtualenv_install_with_resources
+    bin.install_symlink libexec/"bin/fcpscene" => "fcpscene"
+    bin.install "bin/fcpscene-gui"
+  end
+
+  test do
+    system "#{bin}/fcpscene", "--help"
+    assert_predicate bin/"fcpscene-gui", :exist?
+  end
+end
