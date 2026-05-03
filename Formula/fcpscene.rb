5026f8c2555294ddf909ceec68aad6aadf9a5a26
Eric Fortis <efortis@gmail.com>
2026-05-03 05:46:20 -0400

    1.11.1

--- Formula/fcpscene.rb
+++ Formula/fcpscene.rb
@@ -3,8 +3,8 @@ class Fcpscene < Formula
 
   desc "Final Cut Pro scene cut detector"
   homepage "https://github.com/ericfortis/final-cut-pro-scene-detect"
-  url "https://github.com/ericfortis/final-cut-pro-scene-detect/archive/refs/tags/v1.11.0.tar.gz"
-  sha256 "ec7045f8355092ba4d58f5110b697679eee6f7126897215a07667fb5a4640636"
+  url "https://github.com/ericfortis/final-cut-pro-scene-detect/archive/refs/tags/v1.11.1.tar.gz"
+  sha256 "74c4420adf722e0a8c8d7c08547d633beca80aa6b200298fe257a1a26e0f3323"
   license "MIT"
 
   depends_on "python@3.13"
